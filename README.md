f8e4a282e74401d955eccd795898c78d0cafec86
vinaynmcci <vinayn@mcci.com>
2023-07-31 12:33:47 +0530

    brix

--- README.md
+++ README.md
@@ -1,1 +1,85 @@
-# autoupdate
+# BrixUI
+
+Simple standard desktop application for pushing Brix data in a convenient way.
+
+## List of Contents
+
+<!-- TOC depthFrom:2 updateOnsave:true -->
+
+- [About Application](#about-application)
+
+- [Prerequisites for running or building](#prerequisites-for-running-or-building)
+
+- [Interpret python source](#interpret-python-source)
+
+- [GUI Preview](#gui-preview)
+
+- [Meta](#meta)
+  - [Trademarks](#trademarks)
+
+## About Application
+
+This application is designed to create Simple User Interface for pushing the Brix data manually.
+It is a cross platform GUI Application developed by using Wxpython.
+
+## Prerequisites for running or building
+
+<strong>On Windows:</strong>
+
+Development environment
+
+* OS - Windows 10 64 bit
+* Python - 3.7.6
+* wxpython - 4.0.7.post2
+* requests - 2.26
+* pyinstaller - 3.6 
+
+Download [python3.7.6](https://www.python.org/downloads/release/python-376/) and install
+
+```shell
+pip install wxpython==4.0.7.post2
+pip install requests
+pip install pyinstaller
+```
+<strong>On Mac OS:</strong>
+
+Development environment
+
+* Mac OS - Catalina 10.15.7 64 bit
+* Python - 3.6.9
+* wxpython - 4.0.7.post2
+* requests - 2.26
+* pyinstaller - 4.6
+```shell
+sudo apt-get update
+sudo apt-get install python3
+sudo apt-get install python3-pip
+sudo pip3 install wxpython==4.0.7.post2
+sudo pip3 install requests
+```
+
+## Interpret python source
+
+Move to the directory `destdir/src/`
+
+Run the below command
+
+For Windows
+```shell
+python main.py
+```
+For Mac
+```shell
+python3.6 main.py
+```
+## GUI Preview
+
+![UI Preview](assets/BrixUI.png)
+
+
+
+## Meta
+
+### Trademarks
+
+MCCI and MCCI Catena are registered trademarks of MCCI Corporation. All other marks are the property of their respective owners.
---
+++ brix-ui/src/uiGlobal.py
@@ -0,0 +1,76 @@
+##############################################################################
+# 
+# Module: uiGlobal.py
+
+##############################################################################
+# Lib imports
+import wx
+import os
+
+
+IMG_ICON = "mcci_logo.ico"
+IMG_LOGO = "mcci_logo.png"
+
+ID_MENU_FILE_CLOSE  = 1012
+ID_MENU_HELP_ABOUT = 1019
+ID_MENU_HELP_WEB = 1017
+ID_MENU_HELP_PORT = 1018
+ID_MENU_HELP_PDB = 1020
+ID_MENU_HELP_DNC = 1021
+ID_ABOUT_IMAGE = 1033
+ID_MENU_LOGIN = 1034
+
+loginToken = None
+
+root_Url = "https://www.cornellsaprun.com/dncserver/"
+mcci_web = "https://mcci.com/"
+mcci_support = "https://portal.mcci.com/portal/home"
+brixui_dashboard = "https://www.cornellsaprun.com/grafana"
+dncui = "https://www.cornellsaprun.com/dncui/"
+
+##############################################################################
+
+#======================================================================
+# GLOBAL STRINGS
+#======================================================================
+VERSION_NAME  = "\nMCCI"+u"\u00AE"+" Brix UI"
+VERSION_ID    = ""
+VERSION_COPY  = "\nCopyright "+u"\u00A9"+" 2022 MCCI Corporation"
+VERSION_STR = "V1.2.0"
+###############################################################################
+
+class NumericValidator(wx.Validator):
+    
+    def __init__(self):
+        
+        wx.Validator.__init__(self)
+        self.Bind(wx.EVT_CHAR, self.OnChar)
+
+    def Clone(self, arg=None):
+       
+        return NumericValidator()
+   
+    def Validate(self, win):
+        
+        # Returns the window associated with the validator.
+        tc  = self.GetWindow()
+        val = tc.GetValue()
+        return val.isdigit()
+   
+    def OnChar(self, evt):
+        
+        # Returns the window associated with the validator.
+        tc = self.GetWindow()
+        key = evt.GetKeyCode()
+
+        # For the case of delete and backspace, pass the key
+        if (key == wx.WXK_BACK or key == wx.WXK_DELETE  or key > 255):
+            evt.Skip()
+            return
+      
+        elif (chr(key).isdigit() or chr(key) == "."):
+            evt.Skip()
+            return
+
+        
+        
