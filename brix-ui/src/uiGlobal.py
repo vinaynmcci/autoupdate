aba2884e92a7f777ddac2be9fe6357176b5e2cdb
vinaynmcci <vinayn@mcci.com>
2023-11-02 16:40:16 +0530

    auto

--- brix-ui/src/uiGlobal.py
+++ brix-ui/src/uiGlobal.py
@@ -38,7 +38,7 @@ dncui = "https://www.cornellsaprun.com/dncui/"
 VERSION_NAME  = "\nMCCI"+u"\u00AE"+" Brix UI"
 VERSION_ID    = ""
 VERSION_COPY  = "\nCopyright "+u"\u00A9"+" 2022 MCCI Corporation"
-VERSION_STR = "V2.0.0"
+VERSION_STR = "V2.2.0"
 ###############################################################################
 
 def check_version():
